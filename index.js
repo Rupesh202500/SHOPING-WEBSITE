fd0cc15876cd081f8f721d45a402963aeb4159ea
Rupesh Kumar Mishra <rk8272582@gmail.com>
2025-04-29 16:30:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// "Offers" par click hone par
+document.querySelectorAll('.menu-item')[0].addEventListener('click', function() {
+    window.location.href = "https://tech-offer.com/"; // "offers.html" page pe jaayega
+  });
+  
+  // "Customer Service" par click hone par
+  document.querySelectorAll('.menu-item')[1].addEventListener('click', function() {
+    window.location.href = "https://www.iconindia.org/contact-us/"; // "customer-service.html" page pe jaayega
+  });
+  
+  // "Login/SignUp" par click hone par
+  document.querySelectorAll('.menu-item')[2].addEventListener('click', function() {
+    window.location.href = "https://app.link.com/login"; // "login.html" page pe jaayega
+  } );
+  
+  document.getElementById("shopButton").onclick = () => window.location.href = "https://www.shopclues.com/";
